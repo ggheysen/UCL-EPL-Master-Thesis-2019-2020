394d7a84d1723c69eac8e5e6ad3d12914b3c5ada
ggheysen <guillaume.gheysen@student.uclouvain.be>
2020-02-10 23:37:37 +0100

    Correction - misundoorstanding of CUDA

--- src/unpruned_network.py
+++ src/unpruned_network.py
@@ -61,7 +61,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   try:
     # Currently, memory growth needs to be the same across GPUs
-    tf.config.experimental.set_memory_growth(gpus[gpu_i], True)
+    tf.config.experimental.set_memory_growth(gpus, True)
     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
   except RuntimeError as e:
