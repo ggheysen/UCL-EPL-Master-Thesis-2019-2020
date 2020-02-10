eac86b7adceffea6334ab42a25e953e0a4272c7e
guillaume <guillaume.gheysen@student.uclouvain.be>
2020-02-10 15:26:22 +0100

    Unpruned network finished -> ready to be tested on Cassiopee

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ Guillaume Gheysen's Master Thesis : Deep Learning on FPGA
 
 # Usefull command
 first launch a python script to measure metrics about that one. 
+
 python3.7 network.py
 
 Then go to measurement folder (will be created) to observe the different files created.
 To see the the learning, open the logs using tensorboard
+
  tensorboard --logdir=tb_logs
+
+Retrieve weights from a measurement
+model.load _ weights(weights _ log)
--- src/unpruned_network.py
+++ src/unpruned_network.py
@@ -1,6 +1,7 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                                  Import                                      #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
 import tensorflow as tf
 import numpy as np
 import os
@@ -9,9 +10,12 @@ from math import floor, ceil
 import shlex
 import subprocess
 from timeit import timeit
+from tensorflow.keras.models import model_from_json
+
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                                  Setup                                       #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
 #    # 1: Constants
 now = datetime.now().strftime('%b%d_%H-%M-%S')
 model_name = 'CNN'
@@ -38,9 +42,12 @@ if not os.path.exists(tb_dir):
 log_power_consumption = os.path.join(exp_dir, 'power_consumption.csv')
 #Time measurement file
 time_log = os.path.join(exp_dir, 'time.csv')
-#
+#metrics measure file
 metrics_log = os.path.join(exp_dir, 'metrics.csv')
-
+#model summary file
+model_summary_log = os.path.join(exp_dir, 'model_summary.txt')
+#model
+weights_log = os.path.join(exp_dir, 'weights.h5')
 #    # 3: Model parameters
 depth = 4
 start_f = 8
@@ -64,6 +71,7 @@ if gpus:
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                                  Dataset Creation                            #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
 # 1: We extract the CIFAR10 dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
@@ -93,6 +101,7 @@ test_dataset = test_dataset
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                              Model Creation                                  #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+param = 1e-3
 # 1: Blocks definition
 class ConvBlock(tf.keras.Model):
     def __init__(self, num_filters):
@@ -100,8 +109,11 @@ class ConvBlock(tf.keras.Model):
         self.conv2d = tf.keras.layers.Conv2D(filters=num_filters,
                                              kernel_size=(3, 3),
                                              strides=(1, 1),
-                                             padding='same')
-        self.activation = tf.keras.layers.ReLU()  # we can specify the activation function directly in Conv2D
+                                             padding='same',
+                                             kernel_regularizer=tf.keras.regularizers.l2(param), #To limit overfitting
+                                             bias_regularizer=tf.keras.regularizers.l2(param)
+                                             )
+        self.activation = tf.keras.layers.LeakyReLU(alpha=0.3)
         self.pooling = tf.keras.layers.MaxPool2D(pool_size=(2, 2))
 
     def call(self, inputs):
@@ -124,7 +136,9 @@ class CNN_NonPruned(tf.keras.Model):
         self.flatten = tf.keras.layers.Flatten()
         self.classifier = tf.keras.Sequential()
         self.classifier.add(tf.keras.layers.Dense(units=512, activation='relu'))
+        self.classifier.add(tf.keras.layers.Dropout(0.3, seed=SEED))
         self.classifier.add(tf.keras.layers.Dense(units=num_classes, activation='softmax'))
+        self.classifier.add(tf.keras.layers.Dropout(0.2, seed=SEED))
 
     def call(self, inputs):
         x = self.feature_extractor(inputs)
@@ -132,37 +146,73 @@ class CNN_NonPruned(tf.keras.Model):
         x = self.classifier(x)
         return x
 
+
 # 3: Model instance creation
 model_np = CNN_NonPruned(depth=depth,
                       start_f=start_f,
                       num_classes=num_classes)
 model_np.build(input_shape=(None, img_h, img_w, 3)) # Build Model (Required)
+
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                              Model Training                                  #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
 # 1: Setup
 # Loss
 loss = tf.keras.losses.CategoricalCrossentropy()
 # learning rate
-lr = 1e-3
+lr = 5e-4
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 # Validation metrics
 metrics = ['accuracy']
 # Compile Model
 model_np.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
 # 2: Training (with callbacks)
 callbacks = []
 tb_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_dir,
                                              profile_batch=0,
                                              histogram_freq=1)  # if 1 shows weights histograms
 callbacks.append(tb_callback)
+es_callback = tf.keras.callbacks.EarlyStopping(monitor='my_IoU', patience=10, mode = 'max')
+callbacks.append(es_callback)
+lr_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, mode='min') #To avoid plateau
+callbacks.append(lr_plateau)
+
+model_np.fit(train_dataset,
+             epochs=n_epoch,
+             callbacks=callbacks,
+             validation_data=validation_dataset,
+             steps_per_epoch=floor(len(x_train_n)/batch_size),
+             validation_steps=floor(len(x_validation_n)/batch_size)
+            )
+
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 #                              Measurement                                     #
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
 print("Start measurements")
+# 1 : save model summary
+with open(model_summary_log,'w') as fh:
+    # Pass the file handle in as a lambda function to make it callable
+    model_np.summary(print_fn=lambda x: fh.write(x + '\n'))
+    model_np.feature_extractor.summary(print_fn=lambda x: fh.write(x + '\n'))
+    model_np.classifier.summary(print_fn=lambda x: fh.write(x + '\n'))
 
-# 1 : Power consumption
+# 2 : save weights
+model_np.save_weights(weights_log)
+# load weights into new model
+# model_np.load_weights(weights_log)
+# 3 : Accuracy
+result = model_np.evaluate(test_dataset)
+print('metrics measured')
+dictionnary = dict(zip(model_np.metrics_names, result))
+metrics_log_file = open(metrics_log, "w")
+for key in dictionnary:
+    str_metric = key + ', ' + str(dictionnary[key]) + '\n'
+    metrics_log_file.write(str_metric)
+metrics_log_file.close()
+print('metrics file written')
+
+# 4 : Power consumption
 command = 'nvidia-smi' + ' '
 command += '-i' + ' ' + str(gpu_i) + ' '
 command += '--query-gpu=power.draw --format=csv' + ' '
@@ -173,17 +223,6 @@ p = subprocess.Popen(args)
 eval_out = model_np.predict(test_dataset, steps=len(x_test)/batch_size)
 p.terminate()
 print('Power Consumption measured')
-
-# 2 : Accuracy
-result = model_np.evaluate(test_dataset)
-print('metrics measured')
-dictionnary = dict(zip(model_np.metrics_names, result))
-metrics_log_file = open(metrics_log, "w")
-for key in dictionnary:
-    str_metric = key + ', ' + str(dictionnary[key]) + '\n'
-    metrics_log_file.write(str_metric)
-metrics_log_file.close()
-print('metrics file written')
 
 # 3 : Latency and Throughput
 latency = timeit(lambda: model_np.predict(test_dataset, steps=1),
@@ -199,5 +238,3 @@ time_log_file.write(str_latency)
 time_log_file.write(str_throughput)
 time_log_file.close()
 print('Time file writen')
-
-# 4 : Training (in logs)
