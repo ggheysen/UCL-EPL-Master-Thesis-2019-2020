bed0f6acd187057b8a829c0d2764d5075bbf627f
guillaume <guillaume.gheysen@student.uclouvain.be>
2020-02-22 15:04:47 +0100

    fix repo

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from src.test_model import test_model
 from src.models.models import *
 # Get results for Unpruned network
 test_model('Unpruned_network', CNN_NonPruned)
+# Get results for Unstructured pruning
