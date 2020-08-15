0b03f11a5ee59bf565833355665ea1b598d1bb19
Guillaume Gheysen <guillaume.gheysen@student.uclouvain.Be>
2020-08-15 11:50:33 +0200

    relecture

--- doc/Simulation/max_extmem.m
+++ doc/Simulation/max_extmem.m
@@ -3,7 +3,7 @@ ntotlayers = 21;
 nsclayers = 1;
 nbclayers = 17;
 nt11clayers = 2;
-bw_li = 238;
+bw_li = 8+8+11+11+1+3+11+11+2+(32*6);
 max_fmi = 112*112*32;
 max_fmo = 112*112*32;
 max_k11 = 320*1280;
@@ -17,7 +17,7 @@ alpha = Nnp/Npar;
 bw = 1:32;
 maxmem = zeros(1, length(bw));
 for i = 1: length(bw)
-    maxmem(i) = (ntotlayers * bw(i)) + ...
+    maxmem(i) = (ntotlayers * bw_li) + ...
                 (max_fmi * bw(i)) + ...
                 (max_fmo * bw(i)) + ...
                 (nsclayers * max_ksc * bw(i)) + ...
@@ -26,6 +26,7 @@ for i = 1: length(bw)
                 ((nt11clayers + nbclayers) * alpha * max_k11 * (bw(i) + log2(Npar)));
 end
 maxmem = maxmem ./ (8*1024*1024);
+disp(maxmem(16));
 %%
 f = figure('visible','off');
 hold on;
